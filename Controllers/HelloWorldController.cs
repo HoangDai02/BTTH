66999c38490b78e1880a76291dc19274faecefbf
HoangDai02 <dai.hoang668@gmail.com>
2024-03-19 23:19:53 +0700

    first commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    { 
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "sin chao day la dai";
+        } 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
---
+++ Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+using FirstWebMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebMVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Person ps)
+        {
+            string strOutput = " Xin chào " + ps.PersonId + "-" + ps.FullName + "-" +  ps.Address;
+            ViewBag.infoPerson = strOutput;
+            return View();
+        }
+    }
+}
